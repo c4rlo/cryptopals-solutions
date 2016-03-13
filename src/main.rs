478a9834a092f66a039542224c6db178ed7f0aec
Carlo Teubner <carlo.teubner@gmail.com>
2016-03-13 18:12:17 +0000

    Challenges 4 and 5 done
    - Detect single-character XOR
    - Implement repeating-key XOR
    
    Plus a bit of base64 refactoring in preparation for challenge 6.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::iter;
-use std::io::{BufReader,Read,Write};
+use std::io::{BufReader,BufRead,Read,Write};
 use std::fs::File;
 
 fn hexdigit_decode(d: u8) -> u8 {
@@ -15,6 +15,7 @@ fn hexdigit_decode(d: u8) -> u8 {
 }
 
 fn hex_decode(h: &[u8]) -> Vec<u8> {
+    // h.chunks(2).map(|pair| 16 * hexdigit_decode(pair[0]) + hexdigit_decode(pair[1]))
     let mut result = Vec::new();
     for pair in h.chunks(2) {
         result.push(16 * hexdigit_decode(pair[0]) + hexdigit_decode(pair[1]));
@@ -22,32 +23,57 @@ fn hex_decode(h: &[u8]) -> Vec<u8> {
     result
 }
 
-fn b64_encode(b: &[u8]) -> Vec<u8> {
-    let b64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                  abcdefghijklmnopqrstuvwxyz\
-                  0123456789+/".as_bytes();
-    let mut result = Vec::new();
-    for triple in b.chunks(3) {
-        let len = triple.len();
-        let t0 = triple[0];
-        let t1 = if len >= 2 { triple[1] } else { 0u8 };
-        let t2 = if len >= 3 { triple[2] } else { 0u8 };
-        result.push(b64map[(t0 >> 2) as usize]);
-        result.push(b64map[(((t0 & 0x03u8) << 4) | (t1 >> 4)) as usize]);
-        result.push(
-            if len >= 2 {
-                b64map[(((t1 & 0x0fu8) << 2) | (t2 >> 6)) as usize]
-            } else {
-                '=' as u8
-            });
-        result.push(
-            if len >= 3 {
-                b64map[(t2 & 0x3f) as usize]
-            } else {
-                '=' as u8
-            });
+const BASE64CHARS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                                   abcdefghijklmnopqrstuvwxyz\
+                                   0123456789+/";
+
+struct Base64Codec {
+    enc_map: &'static [u8],
+    dec_map: Vec<u8>
+}
+
+impl Base64Codec {
+    fn new() -> Self {
+        let base64bytes = BASE64CHARS.as_bytes();
+        let mut dec_map = Vec::new();
+        dec_map.resize(256, 0u8);
+        for (i, b) in base64bytes.iter().enumerate() {
+            dec_map[*b as usize] = i as u8;
+        }
+        Base64Codec {
+            enc_map: base64bytes,
+            dec_map: dec_map
+        }
     }
-    result
+
+    fn encode(&self, b: &[u8]) -> Vec<u8> {
+        let mut result = Vec::new();
+        for triple in b.chunks(3) {
+            let len = triple.len();
+            let t0 = triple[0];
+            let t1 = if len >= 2 { triple[1] } else { 0u8 };
+            let t2 = if len >= 3 { triple[2] } else { 0u8 };
+            result.push(self.enc_map[(t0 >> 2) as usize]);
+            result.push(self.enc_map[(((t0 & 0x03u8) << 4) | (t1 >> 4)) as usize]);
+            result.push(
+                if len >= 2 {
+                    self.enc_map[(((t1 & 0x0fu8) << 2) | (t2 >> 6)) as usize]
+                } else {
+                    '=' as u8
+                });
+            result.push(
+                if len >= 3 {
+                    self.enc_map[(t2 & 0x3f) as usize]
+                } else {
+                    '=' as u8
+                });
+        }
+        result
+    }
+
+    fn decode(&self, b: &[u8]) -> Vec<u8> {
+        unimplemented!();
+    }
 }
 
 #[allow(dead_code)]
@@ -60,6 +86,11 @@ fn print_data(v: &[u8]) {
 
 fn xor<I1: Iterator<Item=u8>, I2: Iterator<Item=u8>>(x1: I1, x2: I2) -> Vec<u8> {
     x1.zip(x2).map(|x| x.0 ^ x.1).collect()
+}
+
+fn xor_crypt<I1: Iterator<Item=u8>, I2: Iterator<Item=u8> + Clone>(content: I1, key: I2)
+            -> Vec<u8> {
+    xor(content, key.cycle())
 }
 
 fn chardist<I: Iterator<Item=u8>>(bytes: I) -> [f64; 256] {
@@ -94,19 +125,21 @@ struct SingleXorCandidate {
     decryption: Vec<u8>
 }
 
-fn crack_single_xor(ciphertext: &[u8]) -> SingleXorCandidate {
-    let ciphertext_len = ciphertext.len();
-    let corpus_cd = corpus_chardist();
-    let mut best = SingleXorCandidate {
-        badness: std::f64::INFINITY,
-        key_byte: 0u8,
-        decryption: Vec::new()
-    };
+impl SingleXorCandidate {
+    fn new() -> Self {
+        SingleXorCandidate {
+            badness: std::f64::INFINITY,
+            key_byte: 0u8,
+            decryption: Vec::new()
+        }
+    }
+}
+
+fn crack_single_xor(ciphertext: &[u8], corpus_cd: &[f64; 256]) -> SingleXorCandidate {
+    let mut best = SingleXorCandidate::new();
     for i in 0..256 {
         let key_byte = i as u8;
-        let decryption = xor(
-            ciphertext.iter().cloned(),
-            iter::repeat(key_byte).take(ciphertext_len));
+        let decryption = xor(ciphertext.iter().cloned(), iter::repeat(key_byte));
         let badness = chardist_diff(&chardist(decryption.iter().cloned()), &corpus_cd);
         if badness < best.badness {
             best = SingleXorCandidate {
@@ -123,7 +156,7 @@ fn challenge1() {
     let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBs\
                     aWtlIGEgcG9pc29ub3VzIG11c2hyb29t".as_bytes();
     let raw = hex_decode(input);
-    let b64 = b64_encode(&raw);
+    let b64 = Base64Codec::new().encode(&raw);
     // print_data(&b64);
     assert_eq!(expected, b64.as_slice());
     println!("Challenge 1: Success.");
@@ -136,20 +169,47 @@ fn challenge2() {
     let result = xor(
         hex_decode(input1).iter().cloned(),
         hex_decode(input2).iter().cloned());
-    assert_eq!(hex_decode(expected).as_slice(), result.as_slice());
+    assert_eq!(hex_decode(expected), result);
     println!("Challenge 2: Success.");
 }
 
 fn challenge3() {
     let input = hex_decode("1b37373331363f78151b7f2b783431333d\
                             78397828372d363c78373e783a393b3736".as_bytes());
-    let cracked = crack_single_xor(&input);
+    let cracked = crack_single_xor(&input, &corpus_chardist());
     print!("Challenge 3: key={}; ", cracked.key_byte);
     print_data(&cracked.decryption);
+}
+
+fn challenge4() {
+    let corpus_cd = corpus_chardist();
+    let mut best = SingleXorCandidate::new();
+    for line in BufReader::new(File::open("4.txt").unwrap()).lines() {
+        let ciphertext = hex_decode(line.unwrap().as_bytes());
+        let cracked = crack_single_xor(&ciphertext, &corpus_cd);
+        if cracked.badness < best.badness {
+            best = cracked;
+        }
+    }
+    print!("Challenge 4: ");
+    print_data(&best.decryption);
+}
+
+fn challenge5() {
+    let input = "Burning 'em, if you ain't quick and nimble\n\
+                 I go crazy when I hear a cymbal".as_bytes();
+    let expected = hex_decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a262263\
+                               24272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b2028\
+                               3165286326302e27282f".as_bytes());
+    let output = xor_crypt(input.iter().cloned(), "ICE".as_bytes().iter().cloned());
+    assert_eq!(expected, output);
+    println!("Challenge 5: Success.");
 }
 
 fn main() {
     challenge1();
     challenge2();
     challenge3();
+    challenge4();
+    challenge5();
 }
