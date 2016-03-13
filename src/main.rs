253b0e12a0d0a36f297e84099d61f415e1ece4f7
Carlo Teubner <carlo.teubner@gmail.com>
2016-03-13 13:18:18 +0000

    Challenge 3 done (Single-byte XOR cipher)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::iter;
+use std::borrow::Borrow;
+use std::io::{BufReader,Read,Write};
+use std::fs::File;
 
 fn hexdigit_decode(d: u8) -> u8 {
     if '0' as u8 <= d && d <= '9' as u8 {
@@ -49,7 +52,7 @@ fn b64_encode(b: &[u8]) -> Vec<u8> {
 }
 
 #[allow(dead_code)]
-fn print_vec(v: &[u8]) {
+fn print_data(v: &[u8]) {
     let stdout = std::io::stdout();
     let mut out = stdout.lock();
     out.write_all(v).unwrap();
@@ -64,12 +67,13 @@ fn challenge1() {
                     aWtlIGEgcG9pc29ub3VzIG11c2hyb29t".as_bytes();
     let raw = hex_decode(input);
     let b64 = b64_encode(&raw);
-    // print_vec(&b64);
+    // print_data(&b64);
     assert_eq!(expected, b64.as_slice());
     println!("Challenge 1: Success.");
 }
 
 fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
+    assert_eq!(a.len(), b.len());
     a.iter().zip(b).map(|x| x.0 ^ x.1).collect()
 }
 
@@ -84,7 +88,59 @@ fn challenge2() {
     println!("Challenge 2: Success.");
 }
 
+fn corpus_chardist() -> [f64; 256] {
+    let file = BufReader::new(File::open("corpus.txt").unwrap());
+    let bytes = file.bytes().map(|r| r.unwrap());
+    let mut filtered_bytes = bytes.filter(|&b| b != ('\n' as u8));
+    chardist(&mut filtered_bytes)
+}
+
+fn chardist<B, I: Iterator<Item=B>>(bytes: &mut I) -> [f64; 256]
+            where B: Borrow<u8> {
+    let mut counts = [0u64; 256];
+    let mut total = 0u64;
+    for b in bytes { 
+        counts[*b.borrow() as usize] += 1;
+        total += 1;
+    }
+    let totalf = total as f64;
+    let mut result = [0f64; 256];
+    for i in 0..256 {
+        result[i] = (counts[i] as f64) / totalf;
+    }
+    result
+}
+
+fn chardist_diff(a: &[f64; 256], b: &[f64; 256]) -> f64 {
+    a.iter().zip(b.iter()).fold(0f64, |acc, x| acc + (x.0 - x.1).powi(2))
+}
+
+fn challenge3() {
+    let input = hex_decode("1b37373331363f78151b7f2b783431333d\
+                            78397828372d363c78373e783a393b3736".as_bytes());
+    let input_len = input.len();
+    let corpus_cd = corpus_chardist();
+    let mut best_dist = std::f64::INFINITY;
+    let mut best_key = 0u8;
+    let mut best_plaintext = Vec::new();
+    for i in 0..256 {
+        let b = i as u8;
+        let key: Vec<u8> = iter::repeat(b).take(input_len).collect();
+        let cand = xor(input.as_slice(), key.as_slice());
+        let dist = chardist_diff(&chardist(&mut cand.iter()), &corpus_cd);
+        if dist < best_dist {
+            // println!("Best dist now {}", dist);
+            best_dist = dist;
+            best_key = b;
+            best_plaintext = cand;
+        }
+    }
+    print!("Challenge 3: key={}; ", best_key);
+    print_data(best_plaintext.as_slice());
+}
+
 fn main() {
     challenge1();
     challenge2();
+    challenge3();
 }
